16c5315b3cbbacb2ebb1f05854533558229ab2a4
Tarun Uppal <tarunpreetuppal2007@gmail.com>
2020-12-12 11:55:28 +0530

    Voice recognition and whatsapp web

---
+++ speech-recognition/test.py
@@ -0,0 +1,41 @@
+#Python 2.x program for Speech Recognition 
+  
+import speech_recognition as sr 
+import time
+
+
+
+def getSaidWords():
+   #Sample rate is how often values are recorded 
+   sample_rate = 48000
+   #Chunk is like a buffer. It stores 2048 samples (bytes of data)
+   chunk_size = 2048
+   #Initialize the recognizer 
+   r = sr.Recognizer()
+   
+   #use the microphone as source for input. Here, we also specify  
+   #which device to specifically look for incase the microphone  
+   #is not working, an error will pop up saying "device_id undefined" 
+   with sr.Microphone(device_index = 0, sample_rate = sample_rate,  
+                           chunk_size = chunk_size) as source: 
+      #wait for a second to let the recognizer adjust the  
+      #energy threshold based on the surrounding noise level 
+      r.adjust_for_ambient_noise(source) 
+      print("Say Something")
+      time.sleep(0.1)
+      #listens for the user's input 
+      audio = r.listen(source) 
+            
+      try: 
+         text = r.recognize_google(audio) 
+         print("you said: " + text)
+         
+      #error occurs when google could not understand what was said 
+         
+      except sr.UnknownValueError: 
+         print("Google Speech Recognition could not understand audio") 
+         
+      except sr.RequestError as e: 
+         print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+getSaidWords()
--- whatsapp-web/sender.py
+++ whatsapp-web/sender.py
@@ -25,9 +25,11 @@ x_arg = '//span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located(( 
     By.XPATH, x_arg))) 
 group_title.click() 
+print('in contact info')
 inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
 input_box = wait.until(EC.presence_of_element_located(( 
     By.XPATH, inp_xpath))) 
-for i in range(100): 
+print('found box')
+for i in range(10): 
     input_box.send_keys(string + Keys.ENTER) 
-    # time.sleep(1) 
+    time.sleep(1) 
