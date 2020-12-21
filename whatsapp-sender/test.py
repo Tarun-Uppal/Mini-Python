f52f9347cd17e23ab8ba4e2d1e9577d7594568e6
Tarun Uppal <tarunpreetuppal2007@gmail.com>
2020-12-21 13:25:10 +0530

    Cleanup

--- whatsapp-sender/test.py
+++ whatsapp-sender/test.py
@@ -7,11 +7,14 @@ from urllib.parse import quote
 phone_no = '+919819719688'
 message = 'This message was sent using python'
 
-pg.FAILSAFE = False
-parsedMessage = quote(message)
-web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+parsedMessage)
-time.sleep(2)
-width,height = pg.size()
-pg.click(width/2,height/2)
-time.sleep(3)
-pg.press('enter')
+def sendWhatsappMessage(phone_no, message):
+	pg.FAILSAFE = False
+	parsedMessage = quote(message)
+	web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+parsedMessage)
+	time.sleep(2)
+	width,height = pg.size()
+	pg.click(width/2,height/2)
+	time.sleep(3)
+	pg.press('enter')
+
+sendWhatsappMessage(phone_no, message)
